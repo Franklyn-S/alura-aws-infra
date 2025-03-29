1abe54390ae903834f5d60bd3bfac45f6a3d07cf
Franklyn Seabra Rogerio Bezerra <franklyn.bezerra@serpro.gov.br>
2025-03-29 10:05:16 -0300

    Adiciona auto scale e logs

--- src/main/java/com/myorg/AluraAwsInfraApp.java
+++ src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -2,14 +2,20 @@ package com.myorg;
 
 import software.amazon.awscdk.App;
 
+// cdk deploy --parameters Rds:senha=12345678 Rds
 public class AluraAwsInfraApp {
     public static void main(final String[] args) {
         App app = new App();
         var aluraVPCStack = new AluraVPCStack(app, "vpc");
         var clusterStack = new AluraClusterStack(app, "cluster", aluraVPCStack.getVpc());
         clusterStack.addDependency(aluraVPCStack);
+
+        AluraRdsStack rdsStack = new AluraRdsStack(app, "Rds", aluraVPCStack.getVpc());
+        rdsStack.addDependency(aluraVPCStack);
+
         AluraServiceStack aluraServiceStack = new AluraServiceStack(app, "service", clusterStack.getCluster());
         aluraServiceStack.addDependency(clusterStack);
+        aluraServiceStack.addDependency(rdsStack);
         app.synth();
     }
 }
--- src/main/java/com/myorg/AluraServiceStack.java
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -1,11 +1,17 @@
 package com.myorg;
 
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AluraServiceStack extends Stack {
     public AluraServiceStack(final Construct scope, final String id, Cluster cluster) {
@@ -15,7 +21,15 @@ public class AluraServiceStack extends Stack {
     public AluraServiceStack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
         super(scope, id, props);
 
-        ApplicationLoadBalancedFargateService.Builder.create(this, "MyFargateService")
+        Map<String, String> autenticacao = new HashMap<>();
+        autenticacao.put("SPRING_DATASOURCE_URL", "jdbc:mysql://" + Fn.importValue("pedidos-db-endpoint")
+        + ":3306/alurafood-pedidos?createDatabaseIfNotExist=true");
+        autenticacao.put("SPRING_DATASOURCE_USERNAME", "admin");
+        autenticacao.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-senha"));
+
+        IRepository iRepository = Repository.fromRepositoryName(this, "repositorio", "img-pedidos-ms");
+
+        ApplicationLoadBalancedFargateService loadBalancedFargateService = ApplicationLoadBalancedFargateService.Builder.create(this, "MyFargateService")
                 .serviceName("alura-service-ola")
                 .cluster(cluster)           // Required
                 .cpu(256)                   // Default is 256
@@ -24,12 +38,38 @@ public class AluraServiceStack extends Stack {
                 .assignPublicIp(true)
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
-                                .image(ContainerImage.fromRegistry("jacquelineoliveira/ola:1.0"))
+//                                .image(ContainerImage.fromRegistry("jacquelineoliveira/pedidos-ms"))
+                                .image(ContainerImage.fromEcrRepository(iRepository))
                                 .containerPort(8080)
                                 .containerName("App_ola")
+                                .environment(autenticacao)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "PedidosMsLogGroup")
+                                                .logGroupName("PedidosMsLog")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("PedidosMS")
+                                        .build()))
                                 .build())
                 .memoryLimitMiB(512)       // Default is 512
                 .publicLoadBalancer(true)   // Default is true
                 .build();
+
+        ScalableTaskCount scalableTarget = loadBalancedFargateService.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(3)
+                .build());
+
+        scalableTarget.scaleOnCpuUtilization("CpuScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
+
+        scalableTarget.scaleOnMemoryUtilization("MemoryScaling", MemoryUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
     }
 }
